91d3a4e73034c09cd411ec766bd4b8e3f2f2bcf9
Pablo Rey <pabloreysobral@gmail.com>
2019-05-14 09:47:21 +0200

    formatting changes

--- staff/pablo-rey/rest-api/data/duck-api/index.js
+++ staff/pablo-rey/rest-api/data/duck-api/index.js
@@ -1,26 +1,20 @@
-const validate = require('../../common/validate')
-const call = require('../../common/call')
+const validate = require('../../common/validate');
+const call = require('../../common/call');
 
 const duckApi = {
-    __url__: 'https://duckling-api.herokuapp.com/api',
+  __url__: 'https://duckling-api.herokuapp.com/api',
 
-    searchDucks(query) {
-        validate.arguments([
-            { name: 'query', value: query, type: 'string' }
-        ])
+  searchDucks(query) {
+    validate.arguments([{ name: 'query', value: query, type: 'string' }]);
 
-        return call(`${this.__url__}/search?q=${query}`)
-            .then(response => response.json())
-    },
+    return call(`${this.__url__}/search?q=${query}`).then(response => response.json());
+  },
 
-    retrieveDuck(id) {
-        validate.arguments([
-            { name: 'id', value: id, type: 'string' }
-        ])
+  retrieveDuck(id) {
+    validate.arguments([{ name: 'id', value: id, type: 'string' }]);
 
-        return call(`${this.__url__}/ducks/${id}`)
-            .then(response => response.json())
-    }
-}
+    return call(`${this.__url__}/ducks/${id}`).then(response => response.json());
+  },
+};
 
-module.exports = duckApi
+module.exports = duckApi;
--- staff/pablo-rey/rest-api/data/duck-api/index.spec.js
+++ staff/pablo-rey/rest-api/data/duck-api/index.spec.js
@@ -1,16 +1,14 @@
-const duckApi = require('.')
+const duckApi = require('.');
 
 describe('duck api', () => {
-    describe('search ducks', () => {
-        it('should succeed on correct query', () =>
-            duckApi.searchDucks('yellow')
-                .then(ducks => {
-                    expect(ducks).toBeDefined()
-                    expect(ducks instanceof Array).toBeTruthy()
-                    expect(ducks.length).toBe(13)
-                })
-        )
-    })
+  describe('search ducks', () => {
+    it('should succeed on correct query', () =>
+      duckApi.searchDucks('yellow').then(ducks => {
+        expect(ducks).toBeDefined();
+        expect(ducks instanceof Array).toBeTruthy();
+        expect(ducks.length).toBe(13);
+      }));
+  });
 
-    // TODO other cases
-})
+  // TODO other cases
+});
